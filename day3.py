46f7e8ea97c47901817f4c4a16e572d89c7afd14
Aaporva <apoorvaagarwal.cse19@jecrc.ac.in>
2018-05-15 08:38:18 +0530

    15 may morning practice if implementation

---
+++ day3.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import time
+
+'''
+x=input("Please input something : ")
+print(type(x))
+time.sleep(4)
+print(x)
+'''
+
+
+print("Press 1 : To Search on Browser")
+print("Press 2 : To Get Images")
+print("Press 3 : To Get URL")
+print("Press 4 : To Get Current Time")
+print("Press 5 : To Open default browser")
+print("Press 6 : To Scan Network")
+print("Press 7 : To Get Domain Owner Details")
+
+ch=input("Choice = ")
+
+if ch == '1':
+	print("Enter anything")
+elif ch == '2':
+	print("Displaying images")
+elif ch == '3':
+	print(ch)
+elif ch == '4':
+	
+	print(ch)
+elif ch == '5':
+	
+	print(ch)
+elif ch == '6':
+
+	print(ch)
+elif ch == '7':
+
+	print(ch)
+else:
+	print("Invalid Choice")
