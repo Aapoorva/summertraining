a8ca99f17ac6e705102aa7727e0ae6b61e8a055f
Aaporva <apoorvaagarwal.cse19@jecrc.ac.in>
2018-05-17 17:46:10 +0530

    Received data is converted to list

--- recv.py
+++ recv.py
@@ -3,11 +3,19 @@ import  socket
 rec_ip="127.0.0.1"
 rec_port=9999                      
 #  creating a socket - s 
-#                     ipv4         for UDP 
+#                     ipv4           UDP 
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 #  binding ip  and port 
 s.bind((rec_ip,rec_port)) 
 #  receiving data
+input_list=[]
 while True :
 	data=s.recvfrom(1000)
-	print(data)
+	data_input=data[0].decode()			#convert byte to string
+	#space removed and split
+	if data_input.find(" ")!=-1 :
+		data_input=data_input.strip().split()
+		input_list=input_list+data_input
+	else :
+		input_list.append(data_input)
+	print(input_list)
