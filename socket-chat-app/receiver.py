6af0c751dc9364fb3bbfc0a76612abc490642cbd
Aapoorva <apoorvaagarwal.cse19@jecrc.ac.in>
2018-05-22 01:35:44 +0530

    One Way Message Sending - Presentation Improved

--- socket-chat-app/receiver.py
+++ socket-chat-app/receiver.py
@@ -8,5 +8,14 @@ recv_port=9999
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 #now connecting ip and port
 s.bind((recv_ip,recv_port))
+
+msg_decoded=""
+#to be able to send continously
+while True :
+	if msg_decoded.lower().strip() == "bye" :
+		break
 #buffer size ie. max limit data receiver want to receive at 1 time
-print(s.recvfrom(1000))
+	msg_pckt=s.recvfrom(1000)
+	#Decode msg recieved in Unicode to String
+	msg_decoded=msg_pckt[0].decode()
+	print("Message Recieved : ",msg_decoded)
--- socket-chat-app/sender.py
+++ socket-chat-app/sender.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-
 import socket
+
 recv_ip="127.0.0.1"
 recv_port=9999
 #creating socket
 #		            for ipV4	for UDP
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
 #now connecting ip and port
-#	       msg in python3 sendto accepts unicode msg(i.e. in bytes)
-s.sendto("hello".encode(),(recv_ip,recv_port))
+print("****TO EXIT CHAT SEND MSG - BYE****")
+
+msg=""
+#to be able to send continously
+while True:
+	if msg.lower().strip() == "bye" :
+		break
+
+	msg=input("Enter Message : ")
+	#msg in python3 sendto accepts unicode msg(i.e. in bytes)
+	s.sendto(msg.encode(),(recv_ip,recv_port))
