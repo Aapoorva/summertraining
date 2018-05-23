d85bcae63daae9794f376cc0ae5d81047c950682
Aapoorva <apoorvaagarwal.cse19@jecrc.ac.in>
2018-05-23 22:01:42 +0530

    Thread Applied to Chat App Code

--- socket-chat-app/receiver.py
+++ socket-chat-app/receiver.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import socket
+import _thread
 recv_ip="127.0.0.1"
 recv_port=9999
 #creating socket
@@ -8,18 +9,34 @@ s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 #now connecting ip and port
 s.bind((recv_ip,recv_port))
 
-msg_decoded=""
-#to be able to send continously
-while True :
-	if msg_decoded.lower().strip() == "bye" :
-		break
-#buffer size ie. max limit data receiver want to receive at 1 time
-	msg_pckt=s.recvfrom(1000)
-	#Decode msg recieved in Unicode to String
-	msg_decoded=msg_pckt[0].decode()
-	print("Message Recieved : ",msg_decoded)
+#receiving blank msg to get sender ip and port no.
+print("Setting Up...")
+msg_pckt=s.recvfrom(1000)
+print("Now system is ready....")
+print("Welcome")
 
-	#Reply to received Message
-	reply=input("Enter Reply Message : ")
-	#sending reply
-	s.sendto(reply.encode(),(msg_pckt[-1][0],msg_pckt[-1][-1]))
+def recv_msg():
+	#to be able to receive continously using while
+	while True :
+	#buffer size ie. max limit data receiver want to receive at 1 time
+		msg_pckt=s.recvfrom(1000)
+		#Decode msg recieved in Unicode to String
+		msg_decoded=msg_pckt[0].decode()
+		#printing received msg
+		print("\t\t\t",msg_decoded)
+
+def send_msg():
+	#to be able to send continously using while
+	while True :
+		#Reply to received Message
+		reply=input()
+		#sending reply
+		s.sendto(reply.encode(),msg_pckt[1])
+
+#starting threads
+_thread.start_new_thread(recv_msg,())
+_thread.start_new_thread(send_msg,())
+
+#to keep code alive
+while True:
+	pass
--- socket-chat-app/sender.py
+++ socket-chat-app/sender.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
 import socket
-
+import _thread
 recv_ip="127.0.0.1"
 recv_port=9999
 #creating socket
 #		            for ipV4	for UDP
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-#now connecting ip and port
-print("****TO EXIT CHAT SEND MSG - BYE****")
+#sending blank msg to tell reciver sender ip
+s.sendto("".encode(),(recv_ip,recv_port))
+print("Welcome")
 
-msg=""
-#to be able to send continously
+def send_msg() :
+	#to be able to send continously
+	while True:
+		msg=input()
+		#msg in python3 sendto accepts unicode msg(i.e. in bytes)
+		s.sendto(msg.encode(),(recv_ip,recv_port))
+
+def recv_msg() :
+	"to receive msg continously"
+	while True:
+		#recieving reply message
+		reply_pckt=s.recvfrom(1000)
+		#Decode reply from unicode to string
+		reply_decoded=reply_pckt[0].decode()
+		#sending reply message
+		print("\t\t\t",reply_decoded)
+
+#starting threads
+_thread.start_new_thread(send_msg,())
+_thread.start_new_thread(recv_msg,())
+
+#to keep program alive
 while True:
-	if msg.lower().strip() == "bye" :
-		break
-
-	msg=input("Enter Message : ")
-	#msg in python3 sendto accepts unicode msg(i.e. in bytes)
-	s.sendto(msg.encode(),(recv_ip,recv_port))
-
-	#recieving reply message
-	reply_pckt=s.recvfrom(1000)
-	#Decode reply from unicode to string
-	reply_decoded=reply_pckt[0].decode()
-	print("Reply Message : ",reply_decoded)
+	pass
