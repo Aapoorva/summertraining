efd3f5b8795df88633446fda9fbafeb2a6c3ea70
Aapoorva <apoorvaagarwal.cse19@jecrc.ac.in>
2018-05-25 01:21:40 +0530

    audio from microphone converted to text

--- ML/speechRecogGoogle.py
+++ ML/speechRecogGoogle.py
@@ -2,10 +2,12 @@
 
 #to recognize speech using Api or speech engine
 import speech_recognition as sr
+#to commuticate to microphone install pyaudio
 
 #recognizer instance
 r=sr.Recognizer()
 
+''''
 print("Reading audio file")
 #creating audio file manager instance
 jackhammer = sr.AudioFile('jackhammer.wav')
@@ -15,9 +17,18 @@ with jackhammer as source :
 	r.adjust_for_ambient_noise(source)
 #recording data from source to audio
 	audio = r.record(source)
+'''
+#fetching audio from microphone
+mic=sr.Microphone()
+print("Listening ....")
+#capturing microphone input
+with mic as source :
+	#handling noise
+	r.adjust_for_ambient_noise(source)
+	audio = r.listen(source)
 
 print("Converting to text")
 #passing audio to recongnizer
 #recognizer method for recognizing speech from audio source
 #using google web speech api - require internet
-print(r.recognize_google(audio, show_all=True))
+print("You said : ",r.recognize_google(audio))
