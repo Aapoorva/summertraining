0ff6342f57e491f2959a45e0a8e6eb34d02a15e8
Aapoorva <apoorvaagarwal.cse19@jecrc.ac.in>
2018-05-25 01:12:23 +0530

    audio file read and converted to text using google web api

--- ML/speechRecogGoogle.py
+++ ML/speechRecogGoogle.py
@@ -6,7 +6,18 @@ import speech_recognition as sr
 #recognizer instance
 r=sr.Recognizer()
 
+print("Reading audio file")
+#creating audio file manager instance
+jackhammer = sr.AudioFile('jackhammer.wav')
+#content manager opens and reads file stores in source
+with jackhammer as source :
+#removing noice
+	r.adjust_for_ambient_noise(source)
+#recording data from source to audio
+	audio = r.record(source)
+
+print("Converting to text")
+#passing audio to recongnizer
 #recognizer method for recognizing speech from audio source
 #using google web speech api - require internet
-r.recognize_google()
-
+print(r.recognize_google(audio, show_all=True))
